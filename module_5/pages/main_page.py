153dbcbfb5df63845b40b4dfa8714d8f85f54107
hronorog <hronorog@gmail.com>
2020-12-07 23:36:03 +0300

    some error

--- module_5/pages/main_page.py
+++ module_5/pages/main_page.py
@@ -1,10 +1,13 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        # login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")  # 1
+        # login_link = self.is_element_present(*MainPageLocators.LOGIN_LINK)      # 2
+        # login_link = self.is_element_present(By.CSS_SELECTOR, "#login_link")    # 3
         login_link.click()
 
     def should_be_login_link(self):
